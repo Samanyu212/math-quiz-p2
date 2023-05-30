f38d9bd4e4468fffc2e0c2a70587a73e9021a456
Samanyu212 <121870712+Samanyu212@users.noreply.github.com>
2023-05-30 15:33:46 +0530

    Add files via upload

---
+++ Gamepage.js
@@ -0,0 +1,4 @@
+var player_1name=localStorage.getItem("player1_name");
+document.getElementById("player1_name").innerHTML=player_1name;
+var player_2name=localStorage.getItem("player2_name");
+document.getElementById("player2_name").innerHTML=player_2name;
